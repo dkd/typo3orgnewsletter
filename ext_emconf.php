9a21a0986959d33e685e681c78c61714a7f7c6b8
Ivan Kartolo <ivan.kartolo@dkd.de>
2013-08-12 16:55:27 +0200

    added folder structure and HTML Template

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "typo3org_newsletter".
+ *
+ * Auto generated 09-08-2013 19:42
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'typo3.org Newsletter',
+	'description' => 'Template for the typo3.org Newsletter',
+	'category' => 'templates',
+	'author' => 'Ivan Kartolo',
+	'author_email' => 'ivan.kartolo@dkd.de',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"09c6";s:12:"ext_icon.gif";s:4:"1bdc";s:10:"README.txt";s:4:"ee2d";s:19:"doc/wizard_form.dat";s:4:"68ed";s:20:"doc/wizard_form.html";s:4:"03c7";}',
+);
+
+?>
