4f9793b414afc2da1e3ce25f6669f90feee05791
Ivan Kartolo <ivan.kartolo@gmail.com>
2013-08-30 14:36:21 +0200

    added direct_mail as dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,6 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'depends' => array(
 			'php' => '5.3.0-5.3.99',
 			'typo3' => '4.5.0-4.5.99',
+			'direct_mail' => '3.1.0-3.1.99',
 		),
 		'conflicts' => array(
 		),
